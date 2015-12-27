220326814ad4767b4e0068ade4e7f39fffc03144
Ben B <bburdette@gmail.com>
2015-12-27 10:58:46 -0500

    almost compiling; hung up on turning a Task into an Effect

--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,11 @@
 import TestList exposing (init, update, view)
 import StartApp.Simple exposing (start)
 
+port starttests : Signal () 
 
 main =
   start
-    { model = init
+    { model = init starttests
     , update = update
     , view = view
     }
--- src/TestList.elm
+++ src/TestList.elm
@@ -4,21 +4,24 @@ import Test
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-
+import Signal
+import Effects
 
 -- MODEL
 
 type alias Model =
-    { tests : List ( ID, Test.Model )
+    { startmbx: Signal.Mailbox () 
+    , tests : List ( ID, Test.Model )
     , nextID : ID
     }
 
 type alias ID = Int
 
 
-init : Model
-init =
-    { tests = []
+init : Signal () -> Model
+init sig =
+    { startmbx = Signal.mailbox sig
+    , tests = []
     , nextID = 0
     }
 
@@ -30,11 +33,14 @@ type Action
     | Modify ID Test.Action
 
 
-update : Action -> Model -> Model
+
+update : Action -> Model -> (Model, Effects.Effects Action)
 update action model =
   case action of
     StartTests ->
       -- start the tests!
+      -- let mbx = Signal.mailbox model.startsig in
+      
       {-
       let newTest = ( model.nextID, Test.init 0 )
           newTests = model.tests ++ [ newTest ]
@@ -44,7 +50,7 @@ update action model =
               nextID = model.nextID + 1
           }
       -}
-      model
+      (model, Signal.send model.mbx ())
     Modify id testAction ->
       let updateTest (testID, testModel) =
               if testID == id then
@@ -52,7 +58,8 @@ update action model =
               else
                   (testID, testModel)
       in
-          { model | tests = List.map updateTest model.tests }
+         ({ model | tests = List.map updateTest model.tests },
+          Effects.none)
 
 
 -- VIEW
